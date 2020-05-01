4664d59022416d9a2a61aaa652db9d005c6e31a3
ujusy <lsyeon1997@ajou.ac.kr>
2020-05-02 04:48:51 +0900

    로그인 화면 구현 완룟!

--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -10,11 +10,87 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var allButtons: [UIButton]!
+
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var sum : Int? = 0
+    var num : Int? = 0
+    var labelval:String = "";
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLayout()
     }
+    func setLayout(){
+        allButtons.forEach({
+            $0.layer.cornerRadius = $0.layer.frame.size.height  * 0.44
+        })
+        
+        
+    }
+    
+    var tapped = false
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let button = sender.titleLabel?.text
+        
+        if tapped {
+            resultLabel.text = resultLabel.text!+button!
+        }else{
+            resultLabel.text = button!
+            tapped = true
+        }
+        
+    }
+    var op = "+"
+    
+    @IBAction func acButton(_ sender: Any) {
+        sum = 0
+        num = 0
+        resultLabel.text = ""
+    }
+    var isPlus = false
+    @IBAction func plusButton(_ sender: UIButton) {
 
+//        if isPlus == false {
+////            num = Int(resultLabel.text!)
+//            num = (Int(resultLabel.text!)!)
+//            print(num)
+////            sum = num!
+//            tapped = false
+//            isPlus = true
+//        }
+   if isPlus == false {
+               if let tmp_num = (Int(resultLabel.text!)!) {
+                   print(tmp_num)
+                   tapped = false
+                   isPlus = true
+               }
+   //            sum = num!
+               
+           }
+       
+    }
+    
+    @IBAction func equalButton(_ sender: UIButton) {
+//        let num2 = Int(resultLabel.text!)
+//        var result : Int?
+//        switch op{
+//        case "+":
+//            result = num! + num2!
+//        default :
+//            result = 0
+//
+//        }
+        if isPlus{
+            let num2 = Int(resultLabel.text!)
+            sum = num2! + num!
+            }
+        resultLabel.text = String(sum!)
+        sum = 0
 
+    }
 }
 
